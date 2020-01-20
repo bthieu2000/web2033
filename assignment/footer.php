777fbeab48b28fad0f946d77e23045098aad606f
mrjerox <60104219+mrjerox@users.noreply.github.com>
2020-01-20 21:58:09 +0700

    Add files via upload

---
+++ assignment/footer.php
@@ -0,0 +1,90 @@
+		<footer>
+			<div class="container-fluid border-bottom">
+				<div class="ft-banner text-center p-4">
+					<h4 class="lead-4 mt-5">Sign up to <label class="h2"><strong>NEWSLETTER</strong></label> and Get a <label class="h2"><strong>$50 COUPON</strong></label></h4>
+					<div class="d-flex justify-content-center">
+						<div class="p-2 bd-highlight">
+							<form class="form-inline">
+							    <input class="form-control mr-sm-2" type="search" placeholder="Enter your email adress" aria-label="Search">
+							    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+						  	</form>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+					    <div class="col-lg-3 col-sm-12 col-md-12">
+					    	<div class="d-block">
+					    		<h6 class="pt-4 pr-4 pb-4"><b>ABOUT US</b></h6>
+					    		<p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					    		<div class="social-contact">
+					    			<a href="#"><img src="img/small-facebook-icon-image-20.png" width="30rem"></a>
+					    			<a href="#"><img src="img/e106ce9c0551cf84c74882fd07aa4796.png" width="30rem"></a>
+					    			<a href="#"><img src="img//461-4618525_ig-small-instagram-logo-2019-hd-png-download.png" width="30rem"></a>
+					    			<a href="#"><img src="img/368568.png" width="30rem"></a>
+					    		</div>
+					    	</div>
+					    </div>
+					    <div class="col-lg-2 col-sm-6 col-md-6">
+					    	<div class="ft-collum">
+					    		<h6 class="pt-4 pr-4 pb-4 "><b>INFORMATION</b></h6>
+					    		<a href="#">About Us</a>
+					    		<a href="#">Contact Us</a>
+					    		<a href="#">Blogs</a>
+					    		<a href="#">Terms of Use</a>
+					    		<a href="#">Privacy Policies</a>
+					    	</div>
+					    </div>
+					    <div class="col-lg-2 col-sm-6 col-md-6">
+					    	<div class="ft-collum">
+					    		<h6 class="pt-4 pr-4 pb-4 "><b>MY ACCOUNT</b></h6>
+					    		<a href="#">My Order</a>
+					    		<a href="#">Wishlist</a>
+					    		<a href="#">My Credit</a>
+					    		<a href="#">Login</a>
+					    		<a href="#">Personal Info</a>
+					    	</div>
+					    </div>
+					    <div class="col-lg-2 col-sm-6 col-md-6">
+					    	<div class="ft-collum">
+					    		<h6 class="pt-4 pr-4 pb-4 "><b>CUSTOMER</b></h6>
+					    		<a href="#">Helt & FAQ?</a>
+					    		<a href="#">Order & Return</a>
+					    		<a href="#">Shipping Info</a>
+					    		<a href="#">Search Team</a>
+					    		<a href="#">Store Location</a>
+					    	</div>
+					    </div>
+					    <div class="col-lg-2 col-sm-6 col-md-6">
+					    	<div class="ft-collum">
+					    		<h6 class="pt-4 pr-4 pb-4 "><b>STORE 1</b></h6>
+					    		<a href="#">267 Ho Tung Mau street, Cau Dien, Nam Tu Liem, Ha Noi</a>
+					    		<a href="#">+84 008 889 123</a>
+					    		<a href="#">8h -> 18h</a>
+					    		<h6 class="pt-4 pr-4 pb-4 "><b>STORE 2</b></h6>
+					    		<a href="#">267 Ho Tung Mau street, Cau Dien, Nam Tu Liem, Ha Noi</a>
+					    		<a href="#">+84 008 889 123</a>
+					    		<a href="#">8h -> 18h</a>
+					    	</div>
+					    </div>
+					</div>
+				</div>
+			</div>
+			<div class="bottom container">
+				<div class="d-flex justify-content-between">
+				  <div class="p-2 bd-highlight">
+					<p>Coppyright UOS, All rights Reserved</p>
+				  </div>
+				  <div class="p-2 bd-highlight">
+				  </div>
+				  <div class="p-2 bd-highlight">
+				  	<a href="#"><img src="img/small-facebook-icon-image-20.png" width="30rem"></a>
+					<a href="#"><img src="img/e106ce9c0551cf84c74882fd07aa4796.png" width="30rem"></a>
+					<a href="#"><img src="img/461-4618525_ig-small-instagram-logo-2019-hd-png-download.png" width="30rem"></a>
+					<a href="#"><img src="img/368568.png" width="30rem"></a>
+				  </div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ assignment/header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/custom.css">
+		<title>MODUS</title>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<div class="d-flex justify-content-between">
+				  <div class="p-2 bd-highlight">
+					<a href="#"><img src="img/small-facebook-icon-image-20.png" width="30rem"></a>
+					<a href="#"><img src="img/e106ce9c0551cf84c74882fd07aa4796.png" width="30rem"></a>
+					<a href="#"><img src="img/461-4618525_ig-small-instagram-logo-2019-hd-png-download.png" width="30rem"></a>
+					<a href="#"><img src="img/368568.png" width="30rem"></a>
+				  </div>
+				  <div class="p-2 bd-highlight">
+				  </div>
+				  <div class="p-2 bd-highlight">
+				  	<img src="img/mail-icons-free-and-small-mail-icon-white-11563266542oea3uz6rp1.png" width="15rem">
+					<a href="#" class="text-muted h6 mr-3">sondbhb422000@gmail.com</a>
+					<img src="img/clock.png" width="15rem">
+					<a href="#" class="text-muted h6">9:00 AM - 18:00 PM</a>
+				  </div>
+				</div>
+			</div>
+			<div class="container-fluid border-top">
+				<div class="container-md">
+					<div class="d-flex justify-content-between">
+						<div class="p-2 bd-highlight">
+							<h1 class="p-4"><a href="#" style="display: block;color: black;">MODUS</a></h1>
+						</div>
+						<div class="p-2 bd-highlight"></div>
+						<div class="p-2 bd-highlight">
+							<nav class="navbar navbar-expand-lg navbar-light navbar-default">
+							  <div class="collapse navbar-collapse" id="navbarNav">
+							    <ul class="navbar-nav p-4">
+							      <li class="nav-item active pr-3">
+							        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+							      </li>
+							      <li class="nav-item pr-3">
+							        <a class="nav-link " href="#">CATEGORIES</a>
+							      </li>
+							      <li class="nav-item pr-3">
+							        <a class="nav-link" href="#">DEALS</a>
+							      </li>
+							      <li class="nav-item pr-3">
+							        <a class="nav-link" href="#">ACCESSORIES</a>
+							      </li>
+							      <li class="nav-item pr-3">
+							        <a class="nav-link" href="#">BLOG</a>
+							      </li>
+							      <li class="nav-item pr-3">
+							        <a class="nav-link" href="#">CONTACT</a>
+							      </li>
+							      <li class="nav-item pr-3 mt-2">
+							      	<a href="#"><img src="img/account.png" width="30rem"></a>
+							      </li>
+							      <li class="nav-item pr-3 mt-1">
+							      	<a href="#"><img src="img/logo-cart-png-3.png" width="40rem"></a>
+							      </li>
+							    </ul>
+							  </div>
+							</nav>
+						</div>
+					</div>
+				</div>
+				<section class="searching">
+					<div class="container-fluid">
+						<div class="container">
+						  	<form class="form-inline">
+							    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+							    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+								<select class="custom-select ml-1">
+									<option selected>All categories</option>
+									<option value="1">One</option>
+									<option value="2">Two</option>
+									<option value="3">Three</option>
+								</select>
+								<div class="pl-2 pr-3">
+									<img src="img/49775.png" width="50rem">
+								</div>
+								<div class="">
+									<p class="h6"><strong>SUPPORT 24/7</strong></p>
+							     	<label>Hotline:+08 888 888</label>
+								</div>
+						  	</form>
+						</div>
+					</div>
+				</section>
+			</div>
+		</header>

6990f69c4217e5bde0c69abfa70de13f5dfa6257
mrjerox <60104219+mrjerox@users.noreply.github.com>
2020-01-31 11:06:33 +0700

    Add files via upload

---
+++ assignment/categories.php
@@ -0,0 +1,146 @@
+<?php require_once "./header.php" ?>
+<!-- NỘI DUNG VIẾT TRONG CLASS DƯỚI-->
+	<div class="container-fluid">
+		<div class="categories-content">
+			<div class="container">
+				<section class="top-navigation"> 
+					<div class="py-3">
+						<a href="index.php">Home</a> > <label class="">Bedroom</label> 
+	 				</div>
+				</section>
+				<div class="row">
+					<div class="col-sm-12 col-md-3 col-lg-3">
+						<div class="all-categories px-3 pt-3">
+							<h6 class="border-bottom pb-3"><strong>ALL CATEGORIES</strong></h6>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Bedroom</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Kitchen & Dining</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Office</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Bathroom</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Lighting</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Bar</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Baby & Kid</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Game Room</a></p>
+							<p class="border-top pt-2"><a href="#" class="text-dark">Living Room</a></p>
+						</div>
+						<div class="filter mt-4 p-3 border" style="background-color: pink;">
+							<form>
+								<h6 class="pb-3"><strong>CATEGORIES</strong></h6>
+								<div class="form-check pb-3">
+								  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" style="background:red;">
+								  <label class="form-check-label" for="defaultCheck1">Dinning & Bar(2)</label><br>
+								  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+								  <label class="form-check-label" for="defaultCheck1">Office Furniture(2)</label>
+								</div>
+								<h6 class="pb-3"><strong>AVAILABILITY</strong></h6>
+								<div class="form-check pb-3">
+								  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+								  <label class="form-check-label" for="defaultCheck1">In stock(2)</label>
+								</div>
+								<h6 class="pb-3" ><strong>CONDITION</strong></h6>
+								<div class="form-check pb-3">
+								  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+								  <label class="form-check-label" for="defaultCheck1">New(2)</label>
+								</div>
+								<h6 class="pb-3"><strong>MANUFACTURER</strong></h6>
+								<div class="form-check pb-3">
+								  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+								  <label class="form-check-label" for="defaultCheck1">Furniture Manufacturer(2)</label>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="col-sm-12 col-md-9 col-lg-9">
+						<div class="banner-categories">
+							<img src="https://via.placeholder.com/1000x360" width="100%">
+						</div>
+						<div class="sort mt-3">
+							<label class="h5 font-weight-bold">BEDROOM</label><label class="h6 float-right">There are 13 products</label>
+							<form>
+								<label class="text-muted">Sort by</label>
+								<select class="mr-3">
+									<option>--</option>
+									<option>aaa</option>
+									<option>xxxxxxxxbbb</option>
+								</select>
+								<label class="text-muted">Show</label>
+								<select class="">
+									<option>10</option>
+									<option>15</option>
+								</select>
+								<label class="text-muted">per page</label>
+							</form>
+						</div>
+						<div class="product">
+							<div class="row">
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+								<div class="col-4">
+									<img src="https://via.placeholder.com/300x300" width="100%">
+									<p class="h5"><a href="product-details.php">MINI DESK</a></p>
+									<p class="price h5">$100 <label><del class="h6 text-muted">$50</del></label></p>
+								</div>
+							</div>							
+						</div>
+						<div class="row mt-3 border-top border-bottom mb-5">
+				          <div class="col-12">
+				            <div class="page">
+				            	<label class="float-left">Show 1 - 12 of 100 items</label>
+				            	<ul class="">
+					                <li><a href="#">&lt;</a></li>
+					                <li class="active"><span>1</span></li>
+					                <li><a href="#">2</a></li>
+					                <li><a href="#">3</a></li>
+					                <li><a href="#">4</a></li>
+					                <li><a href="#">5</a></li>
+					                <li><a href="#">&gt;</a></li>
+					            </ul>
+				            </div>
+				          </div>
+				        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossoripgin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<?php require_once "./footer.php" ?>
